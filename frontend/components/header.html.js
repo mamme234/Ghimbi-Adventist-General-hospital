8d93ee1940b3b0c584792ef64faee1033cfc4b35
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-29 11:27:42 +0300

    Create header.html.js

---
+++ frontend/components/header.html.js
@@ -0,0 +1,6 @@
+// This file is loaded inline in the HTML pages
+// The header is rendered dynamically by the main.js loadHeader() function
+// This file is just a placeholder for the component structure
+
+// The header is rendered using JavaScript in main.js
+// See main.js loadHeader() function for implementation
