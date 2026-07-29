8f93841cda4cc46dc1110e69a104d111f489617f
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-29 11:28:10 +0300

    Create footer.html.js

---
+++ frontend/components/footer.html.js
@@ -0,0 +1,6 @@
+// This file is loaded inline in the HTML pages
+// The footer is rendered dynamically by the main.js loadFooter() function
+// This file is just a placeholder for the component structure
+
+// The footer is rendered using JavaScript in main.js
+// See main.js loadFooter() function for implementation
